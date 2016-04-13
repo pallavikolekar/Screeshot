1a85865c7aff6220bfb9131c4304e20b1609d3bf
pallavi <pallavik@glam.com>
2016-04-13 12:46:50 +0530

    Added Some Code Changes

--- src/com/spark/test/PallaviSampletest.java
+++ src/com/spark/test/PallaviSampletest.java
@@ -1,5 +1,7 @@
 package com.spark.test;
 
 public class PallaviSampletest {
-
+    public static void main(String args[]) {
+    	System.out.println("I m in Main Function of java");
+    }
 }
--- src/com/spark/test/TestMain.java
+++ src/com/spark/test/TestMain.java
@@ -3,7 +3,7 @@ package com.spark.test;
 public class TestMain {
 	
 	public static void main(String[] args) {
-		System.out.println("Start");
+	//	System.out.println("Start"); //commented out start line
 		System.out.println("This is main Function");
 		org.junit.runner.JUnitCore.main("com.spark.test.QASample");
 	}
