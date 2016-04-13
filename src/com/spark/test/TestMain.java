0387b52147729e7996da6dbde136fcd7735352fb
pallavi <pallavik@glam.com>
2016-04-13 12:43:34 +0530

    Added just simple SYSO sentence in file

--- src/com/spark/test/TestMain.java
+++ src/com/spark/test/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Start");
+		System.out.println("This is main Function");
 		org.junit.runner.JUnitCore.main("com.spark.test.QASample");
 	}
 
